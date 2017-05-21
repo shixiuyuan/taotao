5179dd0e29691ff5b76761f37a7b33bb78326f4f
sxy <731727718@qq.com>
2017-05-21 23:09:47 +0800

    day02

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,13 @@
+package com.taotao.service;
+
+import com.taotao.common.pojo.EUDataGridResult;
+import com.taotao.pojo.TbItem;
+
+/**
+ * Created by xy on 2017/5/20.
+ */
+public interface ItemService {
+
+    TbItem getItemById(long itemId);
+    EUDataGridResult getItemList(int page,int rows);
+}
