9b0bb911127b5996988282c827b75d05201c5b5b
sxy <731727718@qq.com>
2017-05-30 00:21:18 +0800

    从github桌面客户端同步代码,转成使用IDEA继承的VCS来同步github代码

---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/aaa.java
@@ -0,0 +1,10 @@
+package com.taotao.controller;
+
+/**
+ * Created by xy on 2017/5/30.
+ */
+public class aaa {
+    public static void main(String[] args){
+        System.out.println("hello,world");
+    }
+}
